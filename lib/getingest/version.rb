4043b6d5f18e04cd980058401844897f46c1cc13
Davide Santangelo <davide.santangelo@gmail.com>
2025-03-02 17:10:53 +0100

    first commit

---
+++ lib/getingest/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Getingest
+  VERSION = "0.1.0"
+end
---
+++ spec/getingest_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Getingest do
+  it "has a version number" do
+    expect(Getingest::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
